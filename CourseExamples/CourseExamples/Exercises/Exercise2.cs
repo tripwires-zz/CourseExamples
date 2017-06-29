da847277be18cf54de99106b1690efae06dfa26a
tripwires <michel.brouckaert@gmail.com>
2017-06-29 21:57:32 +0200

    added dummy excersise 2

---
+++ CourseExamples/CourseExamples/Exercises/Exercise2.cs
@@ -0,0 +1,17 @@
+﻿using CourseExamples.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseExamples.Exercises
+{
+    class Exercise2 : IExercise
+    {
+        public void Solve()
+        {
+            Console.WriteLine("excersise 2");
+        }
+    }
+}
